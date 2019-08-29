e507536ef68f0cc1246f84c0b6d9643905846543
Gareth Cronin <gareth@cronin.co.nz>
2019-08-29 20:05:34 +1200

    Forgot .mp3 extension

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ fs.readdir(directory, (err, files) => {
     const fullPath = path.join(directory, file);
     let tags = await id3Tags(fullPath);
     console.log(file, tags.disk.no, tags.track.no);
-    fs.renameSync(fullPath, path.join(directory, `${tags.disk.no}-${tags.track.no} ${tags.artist[0]} - ${tags.album}`));
+    fs.renameSync(fullPath, path.join(directory, `${tags.disk.no}-${tags.track.no} ${tags.artist[0]} - ${tags.album}.mp3`));
   });
 });
