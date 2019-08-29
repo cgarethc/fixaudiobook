333c4b61745988789b833552d3b39b4269762049
Gareth Cronin <gareth@cronin.co.nz>
2019-08-29 20:21:29 +1200

    Docs

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # fixaudiobook
+
 Fix audiobook naming left behind by iTunes rip
+
+- Rip the CD audio book in iTunes, setting the author, title, track number, and disc number tags each time a disc is done
+- Set the DIRECTORY environment variable to the location of the ripping
+- Run index.js: `node index.js`
+- All the files will be titled with the disk and track number up front so that they alphanumerically sort correctly
--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+/*
+  Rip the CD audio book in iTunes, setting the author, title, track number, and disc number tags each time a disc is done
+  Set the DIRECTORY environment variable to the location of the ripping
+  Run index.js
+  All the files will be titled with the disk and track number up front so that they alphanumerically sort correctly
+*/
+
 const fs = require('fs');
 const path = require('path');
 const id3Tags = require('read-id3-tags').readID3Tags;
 
+if(!process.env.DIRECTORY){
+  console.log('Set the DIRECTORY environment variable to the path the book was ripped to');
+  process.exit(2);
+}
 
-const directory = process.env.DIRECTORY || '.';
+const directory = process.env.DIRECTORY;
 
 fs.readdir(directory, (err, files) => {
   if (err) throw err;
